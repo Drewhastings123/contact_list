c689c53c052890be2d93b0f7ab1217a8d3ae9a7c
Drew Hastings <drewhastings@Drews-MacBook-Pro.local>
2024-06-14 12:41:04 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
-
 import "./App.css";
+import ContactList from "./components/ContactList";
+import { dummyContacts } from "./components/ContactList";
 
 function App() {
-  return <></>;
+  const [contacts, setContacts] = useState(dummyContacts);
+  console.log("Contacts: ", contacts);
+  return (
+    <>
+      <ContactList />
+    </>
+  );
 }
 
 export default App;
